db3ab10bc40de0ab4819ac5c580ebf43f5a656a6
jaehee <121213522+jay0425@users.noreply.github.com>
2023-04-30 16:58:07 +0900

    0430

--- 01-D-day-counter/script.js
+++ 01-D-day-counter/script.js
@@ -1,18 +1,17 @@
 const messageContainer = document.querySelector("#d-day-message");
-messageContainer.textContent = "D-Day를 입력해주세요.";
+const container = document.querySelector("#d-day-container");
+
+container.style.display = "none";
+messageContainer.innerHTML = "<h3>D-Day를 입력해주세요.</h3>";
 
 const dateForMaker = function () {
   const inputYear = document.querySelector("#target-year-input").value;
   const inputMonth = document.querySelector("#target-month-input").value;
   const inputDate = document.querySelector("#target-date-input").value;
 
-  // const dateFormat = inputYear + "-" + inputMonth + "-" + inputDate;
   const dateFormat = `${inputYear}-${inputMonth}-${inputDate}`;
-  //템플릿 리터럴
 
   return dateFormat;
-
-  // console.log(inputYear, inputMonth, inputDate);
 };
 
 const counterMaker = function () {
@@ -20,19 +19,40 @@ const counterMaker = function () {
   const nowDate = new Date();
   const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0);
   const remaining = (targetDate - nowDate) / 1000;
-  // 수도코드
+
   if (remaining <= 0) {
     // 만약 remaining이 0이라면 '타이머가 종료되었습니다.' 출력
-    console.log("타이머가 종료되었습니다.");
+    container.style.display = "none";
+    messageContainer.innerHTML = "<h3>타이머가 종료되었습니다.</h3>";
+    messageContainer.style.display = "flex";
+    return;
   } else if (isNaN(remaining)) {
     //만약 잘못된 날짜가 들어왔다면 '유요한 시간대가 아닙니다' 출력
-    console.log("유효한 시간대가 아닙니다.");
+    container.style.display = "none";
+    messageContainer.innerHTML = "<h3>유효한 시간대가 아닙니다.</h3>";
+    messageContainer.style.display = "flex";
+    return;
   }
 
-  const remainingDate = Math.floor(remaining / 3600 / 24);
-  const remainingHours = Math.floor(remaining / 3600) % 24;
-  const remainingMin = Math.floor(remaining / 60) % 60;
-  const remainingSec = Math.floor(remaining) % 60;
+  const remainingObj = {
+    remainingDate: Math.floor(remaining / 3600 / 24),
+    remainingHours: Math.floor(remaining / 3600) % 24,
+    remainingMin: Math.floor(remaining / 60) % 60,
+    remainingSec: Math.floor(remaining) % 60,
+  };
 
-  console.log(remainingDate, remainingHours, remainingMin, remainingSec);
+  const documentArr = ["days", "hours", "min", "sec"];
+  const timeKeys = Object.keys(remainingObj);
+
+  let i = 0;
+  for (let tag of documentArr) {
+    document.getElementById(tag).textContent = remainingObj[timeKeys[i]];
+    i++;
+  }
+};
+
+const starter = function () {
+  container.style.display = "flex";
+  messageContainer.style.display = "none";
+  counterMaker();
 };
